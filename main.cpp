b2238ddfe8af5910f2cff1f1a374751987ec8bff
arturasvell <60686715+arturasvell@users.noreply.github.com>
2020-02-05 09:29:00 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,31 +1,67 @@
 #include <iostream>
 using namespace std;
+struct Remelis
+{
+    string base,outer,inner;
+};
 int main()
 {
-
-    string vardas, base, first, second;
-    base="Sveikas, ";
+    Remelis vard;
+    string vardas;
+    int n;
+    vard.base="Sveikas, ";
     cin>>vardas;
+    cout<<"Parasykite eiluciu skaiciu"<<endl;
+    while(n<3)
+    {
+        cin>>n;
+        if(n<3)
+        {
+            cout<<"Eiluciu skaicius per mazas";
+        }
+    }
+    system("cls");
     if(vardas[vardas.size()-1]=='a'||vardas[vardas.size()-1]=='e')
     {
-        base="Sveika, ";
+        vard.base="Sveika, ";
     }
-    base+=vardas+"!";
+    vard.base+=vardas+"!";
 
     //cout<<base<<endl;
-    for(int i=0;i<base.size()+4;i++)
+    for(int i=0;i<vard.base.size()+4;i++)
     {
-        first+="*";
+        vard.outer+="*";
     }
-    first+='\n';
-    second+="*";
-    for(int i=0;i<base.size()+2;i++)
+    vard.outer+='\n';
+    vard.inner+="*";
+    for(int i=0;i<vard.base.size()+2;i++)
     {
-        second+=" ";
+        vard.inner+=" ";
     }
-    second+="*";
-    second+='\n';
-    base="* "+base+" *";
-    base+='\n';
-    cout<<first<<second<<base<<second<<first;
+    vard.inner+="*";
+    vard.inner+='\n';
+    vard.base="* "+vard.base+" *";
+    vard.base+='\n';
+    //cout<<vard.outer;
+    for(int i=0;i<n;i++)
+    {
+        if(i==0)
+        {
+            cout<<vard.outer;
+        }
+        else if(i==n-1)
+        {
+            cout<<vard.outer;
+        }
+        else if(i!=n/2)
+        {
+            cout<<vard.inner;
+        }
+
+        else
+        {
+            cout<<vard.base;
+        }
+    }
+    //cout<<vard.outer;
 }
